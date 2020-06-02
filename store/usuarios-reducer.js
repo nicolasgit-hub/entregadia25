fe3c0357aa70506cb1b997d23c0b6d717e2c3928
nicolasgit-hub <nicolasmmatos@hotmail.com>
2020-06-01 23:09:59 -0300

    entrega dia 25

---
+++ store/usuarios-reducer.js
@@ -0,0 +1,24 @@
+import { ADD_USUARIO, LISTA_USUARIOS } from "./usuarios-actions";
+import Usuario from "../model/Usuario";
+
+
+const estadoInicial = {
+    usuarios: []    
+};
+
+export default(estado = estadoInicial, action) => {
+    switch(action.type)
+    {
+        case LISTA_USUARIOS:
+            return {
+                usuarios: action.usuarios.map(u => new Usuario(u.id.toString(), u.nome, u.telefone, u.imagemURI))
+            }
+        case ADD_USUARIO:
+            const u = new Usuario(action.dadosUsuario.id.toString(), action.dadosUsuario.nome, action.dadosUsuario.telefone, action.dadosUsuario.imagemURI);
+            return {
+                usuarios: estado.usuarios.concat(u)
+            }
+            default:
+                return estado;
+    }
+}
